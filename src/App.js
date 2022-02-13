2534fef5b31e53a85b0e2a7601d074a6c8f17498
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-13 00:33:19 -0600

    added more components and updated app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
-import Project from './components/Project';
 import Footer from './components/Footer';
 
 function App() {
+  const [categories] = useState([
+    {
+
+    }
+  ])
+
   return (
     <div>
+      <Header />
+      <Navigation>
+
+      </Navigation>
+      <Footer />
     </div>
   );
 }
