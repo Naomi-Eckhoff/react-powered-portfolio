2ce7cd3cc7f7b650f4e103a6387278ecfd07b3ff
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-13 02:38:20 -0600

    switched to Header for navigation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React, { useState } from 'react';
-import Navigation from './components/Navigation';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
-  const [categories] = useState([
-    {
-      name: 'About',
-      description: 'About Me'
-    },
-    {
-      name: 'Project',
-      description: 'My Projects'
-    },
-    {
-      name: 'Resume',
-      description: 'My Resume'
-    }
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
       <Header />
-      <main>
-        <Navigation
-          categories={categories}
-          setCurrentCategory={setCurrentCategory}
-          currentCategory={currentCategory}
-          contactSelected={contactSelected}
-          setContactSelected={setContactSelected}
-        ></Navigation>
-        <Footer />
-      </main>
+      <Footer />
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import Navigation from '../Navigation';
 
 function Header() {
+  const [categories] = useState([
+    {
+      name: 'About',
+      description: 'About Me'
+    },
+    {
+      name: 'Project',
+      description: 'My Projects'
+    },
+    {
+      name: 'Resume',
+      description: 'My Resume'
+    }
+  ]);
 
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+  return (
+    <header>
+      <h1>Naomi Eckhoff</h1>
+      <Navigation
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Navigation>
+    </header>
+  )
 
 }
 
