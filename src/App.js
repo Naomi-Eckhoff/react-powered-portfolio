c5e22c33acea6fc7905cb9a33bda3f27f367e3a6
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-13 02:06:30 -0600

    finished setting up app js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,36 @@ import Footer from './components/Footer';
 function App() {
   const [categories] = useState([
     {
-
+      name: 'About',
+      description: 'About Me'
+    },
+    {
+      name: 'Project',
+      description: 'My Projects'
+    },
+    {
+      name: 'Resume',
+      description: 'My Resume'
     }
-  ])
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
       <Header />
-      <Navigation>
-
-      </Navigation>
-      <Footer />
+      <main>
+        <Navigation
+          categories={categories}
+          setCurrentCategory={setCurrentCategory}
+          currentCategory={currentCategory}
+          contactSelected={contactSelected}
+          setContactSelected={setContactSelected}
+        ></Navigation>
+        <Footer />
+      </main>
     </div>
   );
 }
