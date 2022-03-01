1dfc9bb6f32d65b4609f84fb7577e80793307d64
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-28 18:57:56 -0600

    resume swap

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -10,7 +10,7 @@ function Resume() {
         <p>
           This is a link to the
           <a
-            href="https://docs.google.com/document/d/1swPEuC-1ruc4TSuBZE79IPWSdPQ_Vo--Rn-IbtTwqak/edit?usp=sharing"> resume </a>
+            href="https://docs.google.com/document/d/1qWAw8EagYVga-gRFv5bGrEx2AtJVP_nnRWbJURWM07I/edit?usp=sharing"> resume </a>
           I feel only slightly uncomfortable with being publicly available
         </p>
       </div>
