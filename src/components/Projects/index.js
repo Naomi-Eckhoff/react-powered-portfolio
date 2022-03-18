abea54748868ab2819efb8cfc98979a84620c5f2
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-03-17 19:37:10 -0500

    updated resume

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -28,6 +28,7 @@ function Projects() {
           wall lost that
           battle.
         </p>
+        <br />
         <a href="https://github.com/Naomi-Eckhoff/filter-condition-access/tree/main" target="_blank"><p>Repository Link</p></a>
         <h3> My Other Projects</h3>
         <div class="other-projects">
@@ -40,7 +41,18 @@ function Projects() {
                   alt="A google sheets document written to calculate optimal troop formations for an online game" />
               </a>
               <p>This is an excel spreadsheet I spent nearly 6 months developing to optimize warfare in an online game. The math gets incredibly complex and I did not include the regression analysis necessary to find some of the formulas. Other formulas were generated via trial and error. This serves as an example of how my mind works when presented with an extremely large and complex problem. It boastered performance that surpassed or parred the most experienced players in the world. They wanted to keep their formations secret. I gave power to the people.</p>
+              <br />
               <a href="https://docs.google.com/spreadsheets/d/1WPKTcP0T9pA5KS8s5LACuwLPzzkwvt4Z_Opq-1ydHnc/edit?usp=sharing" target="_blank"><p>Deployed Link</p></a>
+            </div>
+            <div>
+              <h3>Subtrackt</h3>
+              <a href="https://quiet-wildwood-89723.herokuapp.com/" target="_blank">
+                <img src={subtrackt} alt="" />
+              </a>
+              <p>Subtrackt was a group project using the MERN (MongoDB, ExpressJS, React, NodeJS) stack to track subscriptions and find tv shows and movies that are available on those subscriptions</p>
+              <br />
+              <a href="https://quiet-wildwood-89723.herokuapp.com/" target="_blank"><p>Deployed Link</p></a>
+              <a href="https://github.com/ramantv/Subscription-Tracker" target="_blank"><p>Repository Link</p></a>
             </div>
             <div>
               <h3>Studious</h3>
@@ -48,26 +60,19 @@ function Projects() {
                 <img src={studious} alt="" />
               </a>
               <p>Studious was a group project designed to connect students and teachers for the purposes of classes and meetings. It makes use of sql for its back end.</p>
+              <br />
               <a href="https://protected-dusk-79081.herokuapp.com/" target="_blank"><p>Deployed Link</p></a>
               <a href="https://github.com/kcbryan10/Studious" target="_blank"><p>Repository Link</p></a>
             </div>
             <div>
-              <h3>Subtrackt</h3>
-              <a href="https://quiet-wildwood-89723.herokuapp.com/" target="_blank">
-                <img src={subtrackt} alt="" />
-              </a>
-              <p>Subtrackt was a group project using the MERN stack to track subscriptions and find tv shows and movies that are available on those subscriptions</p>
-              <a href="https://quiet-wildwood-89723.herokuapp.com/" target="_blank"><p>Deployed Link</p></a>
-              <a href="https://github.com/ramantv/Subscription-Tracker" target="_blank"><p>Repository Link</p></a>
-            </div>
-            <div>
               <h3>Dinner And A Movie</h3>
-              <a href="https://baxters4karma.github.io/recipe-maker/" target="_blank">
+              <a href="https://naomi-eckhoff.github.io/recipe-maker-1/" target="_blank">
                 <img src={mealAndMovie} alt="Meal and Movie Group Project" />
               </a>
               <p>This was the first group project we did at our boot camp. It's fairly basic lacking even a database. It uses local storage for most of it's data purposes.</p>
-              <a href="https://baxters4karma.github.io/recipe-maker/" target="_blank"><p>Deployed Link</p></a>
-              <a href="https://github.com/christinedbaxter/recipe-maker" target="_blank"><p>Repository Link</p></a>
+              <br />
+              <a href="https://naomi-eckhoff.github.io/recipe-maker-1/" target="_blank"><p>Deployed Link</p></a>
+              <a href="https://github.com/Naomi-Eckhoff/recipe-maker-1" target="_blank"><p>Repository Link</p></a>
             </div>
           </table>
         </div>
