0b4222c5f62c44aac6d29b6e7c2c6b900b775004
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-07-21 06:51:27 -0500

    added place holder html to the carousel

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -79,6 +79,72 @@ const items = [
     </a>
   ],
   [
+    <h3>Access Filter Database Form Generator</h3>,
+    <a href="https://github.com/Naomi-Eckhoff/filter-condition-access/tree/main" target="_blank" rel="noreferrer">
+      <img src={filterDatabase}
+        alt="A small program written in VB and SQL to report the status of filters" class="topProject" />
+    </a>,
+    <div>
+      <p>
+        This is my favorite project. Not necessarily because I'm proud of it, but more because it was the single most frustrating thing I've ever made.
+      </p>
+      <p>
+        It takes data gathered on the conditions of roughly 300 filters. With the press of a button marks which filters are going bad, which are bad, and creates a pdf
+        with the relevant data for the team responsible for changing them.
+      </p>
+      <p>
+        It was written using VB and SQL, but Microsoft Access limits SQL in a number of extremely frustrating ways. I cannot count the number of times I got the error message "SQL statement is too complex".
+        Since it was built to link to a form I couldn't build the tables in a way that was good as Access only lets you use one record at a time. Multiplying the filters by the data associated, without normalization,
+        results in about 8000 fields. After normalization I managed to get that down to around 1200 again not good. If it wasn't for that single record rule Access has then it would be 301 fields for storage.
+        There is an additional table that stores information on each filter that doesn't really change with around 300 records and, I think, 18 fields.
+      </p>
+      <p>
+        So 1 table with 1200 fields sounds pretty bad, but it could be worse. It is in fact worse. Access has 3 other rules that ruin your day.
+      </p>
+      <p>
+        Rule 2, tables cannot be more than 255 fields. So, instead of 2 tables, you get to a minimum of 6.
+      </p>
+      <p>
+        Rule 3, if you join 2 tables and access them with a form those 2 table's fields added together cannot be greater than 255 even if you aren't using all of the fields. So, now you're at 8 tables. 
+      </p>
+      <p>
+        Rule 4, Access does not have a transpose function for SQL. So, in order to use the the table with 18 fields, that's actually decent, you have to make a 9th table to
+        execute a homebrew transpose function across the other 7 tables. This table is promptly deleted as it shouldn't have needed to exist in the first place.
+      </p>
+      <p>
+        Making this program was an exercise in bashing my head against a wall, and everytime I came up with something clever Access would tell me I wasn't allowed to do that.
+        So yeah, this is my favorite creation. I like it when the computer fights me. It's just fun.
+      </p>
+    </div>,
+    <a href="https://github.com/Naomi-Eckhoff/filter-condition-access/tree/main" target="_blank" rel="noreferrer">
+      <p>
+        Repository Link
+      </p>
+    </a>
+  ],
+  [
+    <h3>Studious</h3>,
+    <a 
+      href="https://protected-dusk-79081.herokuapp.com/" target="_blank" rel="noreferrer">
+      <img src={studious} alt="" />
+    </a>,
+    <div>
+      <p>
+        Studious was a group project designed to connect students and teachers for the purposes of classes and meetings. It makes use of sql for its back end. Overall it's pretty simple.
+      </p>
+    </div>,
+    <a href="https://protected-dusk-79081.herokuapp.com/" target="_blank" rel="noreferrer">
+      <p>
+        Deployed Link
+      </p>
+    </a>,
+    <a href="https://github.com/kcbryan10/Studious" target="_blank" rel="noreferrer">
+      <p>
+        Repository Link
+      </p>
+    </a>,
+  ],
+  [
     <h3>Studious</h3>,
     <a 
       href="https://protected-dusk-79081.herokuapp.com/" target="_blank" rel="noreferrer">
